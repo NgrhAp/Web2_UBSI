c0b36e99a5b387d82368bcfaf6dce05af93e0476
NgrhAp <ngurahpratama2002@gmail.com>
2022-11-13 15:58:20 +0700

    First commit

---
+++ application/views/v_about.php
@@ -0,0 +1,25 @@
+<section>
+    <h1><?php echo $judul ?><h1>
+    <h4>Nama</h4>
+    <ul type="disc">
+        <li>First name    : Ngurah</li>
+        <li>Last Name : Arya</li>
+    </ul>
+    <br>
+    <h4>address</h4>
+    <ul type="none">
+        <li>Bangka Raya Street</li>
+    </ul>
+ 
+    <h4>Place of birth</h4>
+    <ul type="none">
+        <li>Jekardah</li>
+    </ul>
+    <h4>Fav Singer</h4>
+    <ul type="square">
+        <li>Chris Martin</li>
+        <li>Arash Buana</li>
+        <li>Ardhito Pramono</li>
+        <li>Harry Style</li>
+    </ul>
+</section>
